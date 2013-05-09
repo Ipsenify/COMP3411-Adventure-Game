01c50a51314485f486a7495e62a73ed3402e7a2c
Sam Ipsen <samipsen@gmail.com>
2013-05-10 04:21:08 +1000

    Deleted c stuff.

--- src/pipe.h
+++
@@ -1,8 +0,0 @@
-/*********************************************
- *  pipe.h
- *  Socket Interface for Text-Based Adventure Game
- *  COMP3411 Artificial Intelligence
- *  UNSW Session 1, 2008
-*/
-
-int tcpopen(char *host, int port);
